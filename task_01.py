1fc886bbe884f3d2c69d6382b78114c6bf4d393e
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-04-22 19:24:50 +0000

    task_01

--- task_01.py
+++ task_01.py
@@ -4,4 +4,9 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """This function handles Index and Key Exceptions"""
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Your index/key does not exist'
+        return var1
