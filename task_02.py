7018d4f56e20a550b316daaaee47fc34f6f5e8d8
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-04-22 19:50:01 +0000

    task_02

--- task_02.py
+++ task_02.py
@@ -7,8 +7,14 @@ import datetime
 
 
 class InvalidAgeError(Exception):
+    """Invalid Age"""
     pass
 
+
 def get_age(birthyear):
+    """Raises an error if age is invalid"""
     age = datetime.datetime.now().year - birthyear
-    return age
+    if age < 0:
+        raise InvalidAgeError('Not a valid age')
+    else:
+        return age
